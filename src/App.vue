ccf5a723320705c6dadcc2b203b29f1a11b0b2fc
NEBULA1978 <rpascualey@gmail.com>
2023-08-10 13:36:42 +0200

    Terminada falta corregir css

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <h1>Padre</h1>
-  <FichaVue/>
+  <!-- <h1>Padre</h1> -->
+  <div>
+    <div v-for="(cosa,index) in cosas" :key="index">
+      <FichaVue :cosa="cosa" :index="index" @retorno="retorno"/>
+
+    </div>
+  </div>
+
 
 </template>
 
 
 <script setup>
 import FichaVue from './components/Ficha.vue';
+import cosas from "./assets/datos"
 
-
+const retorno=(a,i)=>{
+  if(a>=6){
+    cosas.value[i].nota=1;
+    
+  }else {
+    cosas.value[i].nota=a+1;
+  }
+}
 </script>
 
 
--- src/components/Ficha.vue
+++ src/components/Ficha.vue
@@ -1,11 +1,48 @@
 <template>
-    <h1>Hijo</h1>
+    <!-- <h1>Hijo</h1> -->
+    <div class="display">
+        <button class="boton">
+            {{ cosa.ropa }} ({{ cosa.marca }})
+        </button>
+        <div class="estrellas">
+            <span v-for="index in cosa.nota" :key="index">
+                <img src="https://www.html6.es/img/estrella.png" alt="">
+            </span>
+        </div>
+        <button @click="incrementarNota(cosa.nota,index)">+</button>
+    </div>
 </template>
 
 
 <script setup>
+const emito=defineEmits(['retorno'])
+const incrementarNota=(a,i)=>{
+    emito('retorno',a,i)
+}
 
+const props = defineProps({
+    cosa: String,
+    index:Number
+})
 </script>
 
-
-<style scoped></style>
+<style scoped>
+.display{
+    display:flex;
+    align-items:center;
+}
+.display img{
+    width: 15px;
+    margin: 1px;
+}
+.diplay button{
+    margin-right: 20px;
+}
+.boton{
+    width: 165px;
+}
+.estrellas{
+    width: 125;
+    height: 22px;
+}
+</style>
